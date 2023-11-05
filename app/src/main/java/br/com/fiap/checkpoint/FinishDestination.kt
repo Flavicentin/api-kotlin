7b19269667336f65d9d1b90ca5ad3a3a1c595ad3
Flavio Vicentin <69365572+Flavicentin@users.noreply.github.com>
2023-11-05 11:41:48 -0300

    add snackbar

--- app/src/main/java/br/com/fiap/checkpoint/FinishDestination.kt
+++ app/src/main/java/br/com/fiap/checkpoint/FinishDestination.kt
@@ -11,17 +11,23 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.fiap.checkpoint.api.ApiAdapter
 import br.com.fiap.checkpoint.api.CountryModel
 import br.com.fiap.checkpoint.api.RetrofitClient
+import br.com.fiap.checkpoint.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class FinishDestination : Fragment() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_finish_destination, container, false)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.CountryRecycleView)
         recyclerView.layoutManager = LinearLayoutManager(context)
@@ -33,17 +39,21 @@ class FinishDestination : Fragment() {
 
         call.enqueue(object : Callback<List<CountryModel>>{
             override fun onResponse(call: Call<List<CountryModel>>, response: Response<List<CountryModel>>) {
+                view?.let {
+                    Snackbar.make(it, "Consulta com sucesso", Snackbar.LENGTH_SHORT).show()
+                }
+
                 var data = response.body()!!
                 adapter = ApiAdapter(context!!, data)
                 recyclerView.adapter = adapter
             }
 
             override fun onFailure(call: Call<List<CountryModel>>, t: Throwable) {
-                val s = "Ocorreu um erro para consultar os paises"
+                view?.let {
+                    Snackbar.make(it, "Ocorreu um erro ao buscar os países", Snackbar.LENGTH_SHORT).show()
+                }
             }
         })
         return view
     }
-
-
 }
